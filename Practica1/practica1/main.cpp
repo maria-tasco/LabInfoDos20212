3cf10d998cfd56bc38e203a0255af124980a0445
“Your <maria.tasco@udea.edu.co>
2021-12-16 11:45:00 -0500

    Desde hace días estaban los demás problemas faltantes menos el 15, 16, y 11, esos no los pude resolver. solo que pensé que ya había subido los cambios anteriores. La única modificación que se hizo el día de hoy fue intenta el problema 15. Los ejercicios hechos antes de la fecha del día de hoy fueron todos a excepcion del 15, 16, 11

--- Practica1/practica1/main.cpp
+++ Practica1/practica1/main.cpp
@@ -8,7 +8,7 @@ int main ()
   int num1 = 0, num2 = 0; // Se decide usar variavble globales en este caso para el ahorro de memoria //¿Lo que se define en un case luego se borra como pasa en las funciones.
   while ( op != 0)
   {
-      cout<<"Seleccione una opcion"<<endl;
+      cout<<endl<<"Seleccione una opcion"<<endl;
       cout<<"2.Para ver un programa que permita determinar la mínima combinación de billetes y monedas para una cantidad de dinero determinada."<<endl;
       cout<<"3.Para ver un programa que debe leer un mes y un día de dicho mes para luego decir si esa combinación de mes y día son válidos."<<endl;
       cout<<"4.Para saber cual numero es mayor"<<endl;
@@ -78,9 +78,42 @@ int main ()
 
           case 4:
           {
-             num1 = 1245; num2 = 345; // numeros base de prueba
+            cout << "Ingrese la hora inicial : "<< endl; cin >> num1;
+            cout << "Ingrese el tiempo que tarda: " << endl; cin >> num2;
+            int min1 = 0, min2 = 0, h1 = 0, h2 = 0, minf = 0, hf = 0, horafinal = 0;
 
+             h1 = num1 / 100 ;
+             cout << h1 << endl;
+             min1 = num1 % 100 ;
+             cout << min1 << endl;
 
+             h2 = num2 / 100 ;
+             cout << h2 << endl;
+             min2 = num2 % 100 ;
+             cout << min2 << endl;
+
+            if ( h1 > 24 )
+                return 0 ;
+            else
+            {
+                minf = min1 + min2;
+                hf = h1 + h2;
+                if (minf > 60)
+                {
+                    minf -= 60;
+                    hf += 1;
+                    cout << hf<< minf << endl;
+                    if ( hf >= 24)
+                    {
+                        hf -= 24;
+                        horafinal = (hf*100) + minf ;
+                        cout << "La hora es: "<< horafinal << " del otro dia" << endl;
+                        return 0;
+                    }
+                }
+            }
+            horafinal = (hf*100) + minf;
+            cout << "La hora es: "<< horafinal << endl;
 
              break;
           }
@@ -163,6 +196,191 @@ int main ()
             num2 -- ;
             cout <<"El primo numero " << num1 << " Es: "<<num2 << endl;
             break;
+          }
+          case 11 :
+          {
+            num2 = 1;
+            num1 = 8; //Numero de prueba
+            //cout << "Ingrese un numero : " << endl; cin >> num1;
+
+            /*
+            int  i = 2, div = 0, aux = 0, rta = 1, numAux = 0;
+            while (num2 <= num1)
+            {
+                if ( num2 % 2 == 0 ) // Si es par
+                {
+                        i=2;
+                        do
+                        {
+                            div = num2 / i;
+                            cout << num2 << "/ " << i << " = " << div << endl;
+                            if ( div != 0)
+                            {
+                                aux ++; // Me cuenta cuántos divisores tiene el numero
+                                rta *=i;
+                            }
+                            i ++;
+                            num2 = num2 - div;
+                        } while ( div != 1);
+                    cout <<"La multiplicacion de los multiplos soy par " << rta << endl;
+                }
+
+                else
+                {
+                    do
+                        {
+                            i = num2;
+                            div = num2 / i;
+                            cout << num2 << "/ " << i << " = " << div << endl;
+                            if ( div != 0)
+                            {
+                                aux ++; // Me cuenta cuántos divisores tiene el numero
+                                rta *=i;
+                            }
+                            i ++;
+                        } while ( div != 1);
+                    cout <<"La multiplicacion de los multiplos soy impar " << rta << endl;
+                }
+                aux = 0;
+                num2 ++ ;
+                cout << "NUm2: "<< num2 << endl;
+            }*/
+            int  i = 1, div = 0, aux = 0, divAux = 1, numAux = 0;
+            int contAux = 0, iAux = 1, divAux2 = 1, auxAux = 0, num2Aux = 1;
+            //num2 va a tener los factores primos, num1 es el primer numero que se va a descomponer en factores primos e irá disminuyendo hasta llegar a 1
+            while (num1 != 1)
+            {
+                do
+                {
+                    div = num2 % i;
+                    if ( div == 0)
+                        aux ++; // Me cuenta cuántos divisores tiene el numero
+                    i ++;
+                } while ( i <= num2);
+
+                if ( aux == 2)
+                {
+                    cout << num2 << endl; //Factor primo el cuál va a dividir el num1 en el que se encuentre
+                    cout << "num1: "<< num1 << endl;
+                    numAux = num1;
+
+                    while ( numAux > 1)
+                    {
+
+                        cout << "numAUx antes de la división: "<< numAux << endl;
+                        divAux = numAux / num2 ;
+                        cout << "La division : " << numAux << " / " << num2 << " = " << divAux << endl;
+                        if (numAux % num2 == 0 ) //SI se produce una división entera
+                        {
+                            numAux  = divAux;
+                            cout << "numAux toma el valor de la división:  " << numAux << endl;
+
+                        }
+                         else
+                        {
+                            num2Aux = num2; //Toma el valor del primo anterior
+                            while (auxAux != 2)
+                            {
+                                do
+                                {
+                                    divAux2 = num2Aux % iAux;
+                                    if ( divAux2 == 0)
+                                        auxAux ++; // Me cuenta cuántos divisores tiene el numero
+                                    iAux ++;
+                                }while ( iAux <= num2Aux);
+                                iAux =1; num2Aux ++;
+                                if ( auxAux == 2)
+                                {
+                                    contAux ++; // cuenta los numeros primos
+                                }
+                            }
+                            num2 = num2Aux; //Toma el valor del siguiente primo
+                            cout << "nuevo numero primo: " << num2 << endl;
+                        }
+                    }
+                    num1 --;
+                }
+                aux = 0; i=1; num2 ++;
+            }
+
+            num2 -- ;
+            break;
+          }
+
+          case 13 :
+          {
+          num2 = 0;
+          //num2 lo usaremos como posible numero primo y es el que al final se sacará
+          cout << "ingrese un numero: " << endl; cin>> num1;
+          if (num1 < 0)
+          {
+              cout << "La suma empieza conun numero mayor que 0: " << endl;
+              return 0;
+          }
+          int  i = 1, div = 0, aux = 0, suma = 0;
+          while (num2 != num1)
+          {
+              do
+              {
+                  div = num2 % i;
+                  if ( div == 0)
+                      aux ++; // Me cuenta cuántos divisores tiene el numero
+                  i ++;
+              } while ( i <= num2);
+
+              if ( aux == 2)
+              {
+                  cout << num2 << endl;
+                  suma+= num2;
+              }
+              aux = 0; i=1; num2 ++;
+          }
+          num2 -- ;
+          cout <<"La resultado de la suma es:" << suma<< endl;
+            break;
+          }
+          case 15 :
+          {
+            int g=0, suma = 0;
+            //cout << "Ingrese un numero impar: " << endl ; cin >> num1 ;
+            num1 = 5;
+            for ( unsigned short f=0, ts=0, ti = 0; f<num1; f++)
+            {
+                for (unsigned short c = 0; c < num1 ; c++)
+                {
+                    if (f <= c && f <= num1-1-c ) // Triangulo superior
+                    {
+                         cout << (num1-2*f)*(num1-2*f)-(num1-2*f-1)+ts << '\t';
+                         ts ++;
+                    }
+                    else if (f > c && f > num1-1-c) //Triangulo inferior
+                    {
+                        g = num1 -1-f;
+                        ts ++;
+                        cout << (num1-2*g)*(num1-2*g)-2*(num1-2*g-1)-ts << '\t';
+                    }
+                    else if (f>c && f <= num1-1-c)// Triangulo izquierdo
+                    {
+                        cout << (num1-2*c)*(num1-2*c)-(num1-2*c-1)-ti+c << '\t';
+                    }
+                    else
+                    {
+                        g = num1 -c;
+                        cout << (num1-2*g)*(num1-2*g)+ti-(g-1) << '\t';
+
+                    }
+                }
+                ts = 0;
+                ti++;
+                cout << endl << endl << endl;
+            }
+            suma --;
+            cout << "En una espiral de: "<< num1 << "x" << num1 << "La suma de la diagonal es: " << suma;
+            break;
+          }
+          case 16:
+          {
+              break;
           }
       }
 
